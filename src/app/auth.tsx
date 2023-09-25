9548c669508e4e79e403a80a1aca35e8913a4f86
Nelson Faiçal Rechdan <rechdanfr@gmail.com>
2023-09-25 15:58:50 -0300

    Fix missing type

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -47,7 +47,7 @@ const Login = () => {
 
   const onSubmit = useCallback<SubmitHandler<FormData>>(async (data) => {
     try {
-      const { data: responseData } = await api.post<Api.User.Login>("/user/login", data);
+      const { data: responseData } = await api.post<ApiUserLogin>("/user/login", data);
 
       if (responseData) {
         const { jwt, error } = responseData;
